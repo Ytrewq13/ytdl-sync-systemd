e9dda58604d86366f793b0dac912610e94c4a6a8
Sam Whitehead <sam.everythingcomputers@gmail.com>
2022-08-22 01:18:06 +0100

    Add support for custom output template for each rootdir
    to use: add a file named "template.txt" into the rootdir containing only
    the part of the template which comes after the directory. No protection
    against escaping from the target directory so this is probably unsafe
    AF. Use with caution.
    
    Also rename the second rootdir in the example config file

--- download_target.sh
+++ download_target.sh
@@ -2,6 +2,7 @@
 
 YTDL_EXE="${YTDL_EXE:-yt-dlp}"
 ARCHIVE_FILENAME="${ARCHIVE_FILENAME:-archive.txt}"
+TEMPLATE_FILENAME="${TEMPLATE_FILENAME:-template.txt}"
 
 TARGET_CONFIG_FILENAME="target.txt"
 
@@ -15,6 +16,10 @@ dest_template="$target_dir/%(webpage_url_domain)s - [%(id)s] - %(release_date>%Y
 
 # TODO: print a warning message before exiting
 [ -f "$target_file" ] || exit 1
+
+# Read template file from parent directory of target dir
+template_path="$(realpath "$target_dir/..")/$TEMPLATE_FILENAME"
+[ -f "$template_path" ] && dest_template="$target_dir"/"$(cat "$template_path")"
 
 name="$(sed -n '1p' "$target_file")"
 url="$(sed -n '2p' "$target_file")"
